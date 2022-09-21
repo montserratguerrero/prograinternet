bde47930f51eb1056d2456b04dc66bcf387d6784
montserratguerrero <guerrero.montse06@gmail.com>
2022-09-21 18:51:07 -0500

    prograinternet

---
+++ server/main.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var app = express();
+
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+
+var messages = [{
+    
+        id: 1,
+        text: "Hola", 
+        author: "Montse"
+}];
+
+app.use(express.static('public'));
+
+app.get('/hello', function(req, res) {
+    res.status(200).send("Hello, mundo!");
+});
+
+io.on('connection', function(socket) {
+    console.log('Alguien se ha conectado'); 
+    socket.emit('messages', messages);
+    
+    socket.on('new-message', function(data) {
+        messages.push(data);
+
+        io.sockets.emit('messages', messages);
+    });
+});
+
+
+server.listen(8080, function () {
+    console.log("Servidor corriendo en http://localhost:8080");
+  });
